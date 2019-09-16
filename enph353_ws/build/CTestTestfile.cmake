0ccc497cf0e9fb85b05f38220c2937c374856972
Gosha Maruzhenko <gmaruzhenko@yahoo.ca>
2019-09-16 13:39:09 -0700

    added rest

---
+++ enph353_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/gosha/Code/ENPH353-UBC-Parking-Competition/enph353_ws/src
+# Build directory: /home/gosha/Code/ENPH353-UBC-Parking-Competition/enph353_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
