d7a3cc80389dca261dfc995b512b608f7bdc7eba
Gosha Maruzhenko <gmaruzhenko@yahoo.ca>
2019-09-28 10:19:47 -0700

    running cometition sim - make sure to update bashrc

---
+++ enph353_ws/build/CTestTestfile.cmake
@@ -0,0 +1,15 @@
+# CMake generated Testfile for 
+# Source directory: /home/gosha/Code/ENPH353-UBC-Parking-Competition/enph353_ws/src
+# Build directory: /home/gosha/Code/ENPH353-UBC-Parking-Competition/enph353_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("enph353_2019F_competition/adeept_awr/adeept_awr")
+subdirs("enph353_2019F_competition/adeept_awr/adeept_awr_description")
+subdirs("enph353_2019F_competition/enph353/enph353")
+subdirs("enph353_2019F_competition/enph353/enph353_npcs")
+subdirs("enph353_2019F_competition/enph353/enph353_utils")
+subdirs("enph353_2019F_competition/adeept_awr_ros_driver")
+subdirs("enph353_2019F_competition/adeept_awr/adeept_awr_gazebo")
+subdirs("enph353_2019F_competition/enph353/enph353_gazebo")
---
+++ enph353_ws/build/enph353_2019F_competition/enph353/enph353/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/gosha/Code/ENPH353-UBC-Parking-Competition/enph353_ws/src/enph353_2019F_competition/enph353/enph353
+# Build directory: /home/gosha/Code/ENPH353-UBC-Parking-Competition/enph353_ws/build/enph353_2019F_competition/enph353/enph353
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
